b708a0145e6b6f6288819a2969dc6656440760d0
imrismaa <rismasaputri@mail.ugm.ac.id>
2023-08-12 12:31:50 +0700

    First Commit

--- resources/views/struktur.blade.php
+++ resources/views/struktur.blade.php
@@ -20,7 +20,7 @@
                     <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-arrow-left" viewBox="0 0 16 16">
                         <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"/>
                     </svg>
-                    Kembali
+                    Kembali awawd
                 </a>
                 <a class="navbar-brand" href="#">
                     <img src="{{ getAssetDir('assets/image/logo.png') }}" alt="" width="120" height="24"
