78effecaf745d164a4b4178de22b7522317325c3
kafa123 <nurmuhammadkafabih2003@mail.ugm.ac.id>
2023-08-12 12:23:33 +0700

    Second Commit

--- resources/views/struktur.blade.php
+++ resources/views/struktur.blade.php
@@ -44,7 +44,7 @@
                         <div class="struktur">
                             <h6 class="fw-bold text-success struktur judul">
                                 &mdash;&mdash; 
-                                STRUKTUR ORGANISASI oooasdasd
+                                STRUKTUR ORGANISASI oooasdasdasdasdsada
                             </h6>
                             <h1 class="display-5 fw-bold lh-1 mb-3 subjudul">
                                 Yuk kenali struktur organisasi di ASSETS
