41f118d79c67a82f4000131bd80833096651c59e
kafa123 <nurmuhammadkafabih@mail.ugm.ac.id>
2023-11-04 23:35:40 +0700

    testimoni 35%

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -4,10 +4,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>@yield('title')</title>
-    {{-- link css --}}
-    <link rel="stylesheet" href="{{asset('css/orion.css')}}">
+    <title>@yield('title') | Assets</title>
+
+    <link rel="stylesheet" href="{{asset('assets/style/mystyle.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/style/nextpage.css')}}">
+
+    @yield('css')
+
+    {{-- bootstrap --}}
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
     {{-- monsserat --}}
     <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -17,4 +23,5 @@
     @yield('content')
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
+
 </html>
--- resources/views/testimoni.blade.php
+++ resources/views/testimoni.blade.php
@@ -1,12 +1,15 @@
-@extends('layouts.dump2')
+@extends('layouts.master')
 @section('title',"TESTIMONI")
+@section('css')
+    <link rel="stylesheet" href="{{asset("css/testimoni.css")}}">
+@endsection
 @include('components.navbar')
 @section('content')
-<h1 class="text-center fw-bold p-4">Apa Kata Mereka Tentang ASSETS?</h1>
-<div class="container-md mx-4">
-    <div class="row row-cols-1 row-cols-md-2 g-4">
+<h1 class="text-center fw-bold p-4 text-title">Apa Kata Mereka Tentang <span> <img src="{{asset('images/ASSETS.svg')}}" alt=""></span></h1>
+<div class="container-xxl p-5">
+    <div class="row row-cols-1 row-cols-lg-2 g-5">
         <div class="col">
-            <div class="card mb-3" >
+            <div class="card mb-3  bg-green-1" >
                 <div class="row g-0">
                   <div class="col">
                     <img src="{{asset('assets/image/bapakmu.jpg')}}" class="img-fluid rounded-start p-4 " alt="...">
@@ -21,7 +24,22 @@
             </div>
         </div>
         <div class="col">
-            <div class="card mb-3" >
+            <div class="card mb-3 rounded-5 bg-green-2" >
+                <div class="row g-0 rounded-5">
+                  <div class="col-md">
+                    <img src="{{asset('assets/image/bapakmu.jpg')}}" class="img-fluid rounded-start p-4" alt="...">
+                  </div>
+                  <div class="col-md">
+                    <div class="card-body">
+                      <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat ex eaque sed totam sunt, quisquam, illo maxime exercitationem iusto officia sequi natus impedit tenetur quod! Cum vitae ut quae in.</p>
+                      <p class="card-text text-end"><small class="text-body-secondary">-Bang daniel</small></p>
+                    </div>
+                  </div>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card mb-3 bg-green-2" >
                 <div class="row g-0">
                   <div class="col-md">
                     <img src="{{asset('assets/image/bapakmu.jpg')}}" class="img-fluid rounded-start p-4" alt="...">
@@ -36,22 +54,7 @@
             </div>
         </div>
         <div class="col">
-            <div class="card mb-3" >
-                <div class="row g-0">
-                  <div class="col-md">
-                    <img src="{{asset('assets/image/bapakmu.jpg')}}" class="img-fluid rounded-start p-4" alt="...">
-                  </div>
-                  <div class="col-md">
-                    <div class="card-body">
-                      <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat ex eaque sed totam sunt, quisquam, illo maxime exercitationem iusto officia sequi natus impedit tenetur quod! Cum vitae ut quae in.</p>
-                      <p class="card-text text-end"><small class="text-body-secondary">-Bang daniel</small></p>
-                    </div>
-                  </div>
-                </div>
-            </div>
-        </div>
-        <div class="col">
-            <div class="card mb-3" >
+            <div class="card mb-3 bg-green-1" >
                 <div class="row g-0">
                   <div class="col-md">
                     <img src="{{asset('assets/image/bapakmu.jpg')}}" class="img-fluid rounded-start p-4" alt="...">
