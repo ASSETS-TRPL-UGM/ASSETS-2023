5de20fc01e351f0e68bb41b29dd5cb8e1308ae02
imrismaa <rismasaputri@mail.ugm.ac.id>
2024-01-05 13:41:24 +0700

    update card content, bug fixing, delete struktur2

--- resources/views/layouts/dump2.blade.php
+++ resources/views/layouts/dump2.blade.php
@@ -11,7 +11,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet"
      integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
 
-    <link rel="stylesheet" href="{{ getAssetDir('assets/style/mystyle.css') }}">
+    <link rel="stylesheet" href="{{asset('assets/style/mystyle.css') }}">
     <link rel="stylesheet" href="{{ getAssetDir('assets/style/nextpage.css') }}">
     <link rel="stylesheet" href="{{getAssetDir('assets/style/masonry.css')}}">
     <link rel="stylesheet" href="{{asset('css/testimoni.css')}}">
